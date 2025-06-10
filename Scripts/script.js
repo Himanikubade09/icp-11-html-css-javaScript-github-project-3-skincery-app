e0013b5d11b0afc15700d8ebefd18e0ab7bcb0b0
Sumitkumbhar15 <sumitkumbhar204@gmail.com>
2025-06-10 10:26:28 +0530

    navigation btn and age restriction added

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,14 @@
-for(i=1; i <=10; i++ ){
-    console.log(i);
-} 
+const name = prompt("Enter your name: ");
+const agelimit = prompt("Enter your age: ");
+
+if(agelimit === null || agelimit === "")
+{
+    alert(`Hii ${name} ! You didn't enter your age.`);
+}
+else if (agelimit < 18)
+{
+    alert(`Hii ${name}  your age is  ${agelimit} ! You are not allowed to use this product.`);
+}
+else{
+    alert(`Hii ${name}  your age is  ${agelimit}. You are allowed to use this product`);
+}
